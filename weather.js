83c4ab77efe07ef13dedbb132616d4a94a8136c5
Ansharora124 <ansharora13b@gmail.com>
2025-09-04 11:41:55 +0530

    weather images changes acc to weather

--- weather.js
+++ weather.js
@@ -1,16 +1,42 @@
 const apiKey ="f223f88d2a3f64bf71ddcc2331608e1c";
-const apiUrl= "https://api.openweathermap.org/data/2.5/weather?units=metric&q=delhi";
+const apiUrl= "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather() {
-  const response =await fetch(apiUrl + `&appid=${apiKey}`);
+const searchBox=document.querySelector('.search input');
+const searchButton=document.querySelector('.search button');
+const weatherIcon=document.querySelector('.image img');
+
+
+async function checkWeather(city) {
+  const response =await fetch(apiUrl + city + `&appid=${apiKey}`);
   var data = await response.json();
   console.log(data);
   
+
+document.querySelector('.season').innerHTML=data.weather.main;
 document.querySelector('.city').innerHTML=data.name;
 document.querySelector('.temp').innerHTML=Math.round(data.main.temp) + ' °C';
 document.querySelector('.wind').innerHTML=data.wind.speed + ' km/hr';
 document.querySelector('.humidity').innerHTML=data.main.humidity + ' %';
 
+if(data.weather[0].main=="Clouds"){
+  weatherIcon.src="cloud.jpeg";
+}
+else if(data.weather[0].main=="Rain"){
+weatherIcon.src="rain.png";
+}
+else if(data.weather[0].main=="Clear"){
+weatherIcon.src="sun.webp";
+}
+else if(data.weather[0].main== "Drizzle"){
+  weatherIcon.src="drizzle.jpeg";
 
 }
-checkWeather();
+else if(data.weather[0].main=="Mist")
+{
+  weatherIcon.scr="mist.jpeg";
+}
+}
+searchButton.addEventListener('click', ()=>{
+checkWeather(searchBox.value);
+});
+
